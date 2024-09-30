095e694e0d80f8ac789065555dd931b25ef7f1bf
Jones <t0jone12@louisville.edu>
2024-09-30 15:51:47 -0400

    Update with UMAP/TSNE Function

---
+++ R/calc_umap_tsne.R
@@ -0,0 +1,50 @@
+#' Calculate UMAP and TSNE
+#'
+#' @param spe spe object
+#' @param email_address email address to send updates
+#'
+#' @return spe object
+#' @export
+#'
+#' @examples
+#'
+#' calc_umap_tsne(spe, 't0jone12@louisville.edu')
+#'
+calc_umap_tsne <- function(spe, email_address) {
+
+  if (!"gmail.send" %in% names(gmailr::gm_scopes())) {
+    stop("must have permissions to send emails")
+
+  }
+  email <- gmailr::gm_mime() |>
+    gmailr::gm_to({{ email_address }}) |>
+    gmailr::gm_from("bccbiostatsemailer@gmail.com") |>
+    gmailr::gm_subject("UMAP/TSNE Has Begun") |>
+    gmailr::gm_text_body("Calculating UMAP and TSNE has begun")
+
+  gm_send_message(email)
+
+  begin <- Sys.time()
+
+  spe <- scater::runUMAP(spe,
+                         subset_row = rowData(spe)$use_channel,
+                         exprs_values = "exprs")
+
+  spe <- scater::runTSNE(spe,
+                         subset_row = rowData(spe)$use_channel,
+                         exprs_values = "exprs")
+
+  end <- Sys.time()
+
+  email2 <- gmailr::gm_mime() |>
+    gmailr::gm_to({{ email_address }}) |>
+    gmailr::gm_from("bccbiostatsemailer@gmail.com") |>
+    gmailr::gm_subject("UMAP/TSNE Has Finished") |>
+    gmailr::gm_text_body(paste0("UMAP and TSNE is done. Time: ",
+                                end-begin) )
+
+  gm_send_message(email2)
+
+  return(spe)
+
+  }
